6bcdc860fd83d6a0f976ae66daf0fafc97645def
Aummii Desu <74613199+Phornhathai@users.noreply.github.com>
2024-01-04 11:47:32 +0700

    update 2 functions to common functions

--- mini-project/src/pages/home/Home.tsx
+++ mini-project/src/pages/home/Home.tsx
@@ -3,14 +3,13 @@ import { signOut, onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../libs/firebase";
 import { ChangeEvent, useEffect, useState } from "react";
 import "./home.scss";
-import FileUpload from "../../components/fileUpload/FileUpload";
-import ShowPDF from "../../components/showPDF/showPDF";
-import Base64ToPDF from "../../components/ิBase64toPDF/base64ToPDF";
-import { PDFtobase64 } from "../../services/CommonFunction";
+import { PDFtobase64, base64toPDF } from "../../services/CommonFunction";
 
 const Home = () => {
   // สร้าง state เพื่อเก็บข้อมูลผู้ใช้ที่ login
   const [user, setUser] = useState<any>(null);
+  const [iframeSrcs, setIframeSrcs] = useState<string[]>([]);
+
   const navigate = useNavigate();
 
   useEffect(() =>
@@ -52,6 +51,18 @@ const Home = () => {
     }
   };
 
+  const handleBase64toPDF = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const inputText = e.target.value;
+    const base64Strings = inputText
+      .split("\n")
+      .map((line) => line.trim())
+      .filter(Boolean);
+
+    base64toPDF(base64Strings).then((srcs: string[]) => {
+      setIframeSrcs(srcs);
+    });
+  };
+
   return (
     <form onSubmit={handleLogout} className="home">
       {/* create left box for showing information */}
@@ -66,13 +77,32 @@ const Home = () => {
           </div>
         )}
       </div>
-      <div className="Info">
-        <input
-          type="file"
-          name="pdfUpload"
-          id="pdfUpload"
-          onChange={handlePDFtobase64}
-        />
+      <div className="">
+        <div className="uploadPDFfile">
+          <input
+            type="file"
+            name="pdfUpload"
+            id="pdfUpload"
+            onChange={handlePDFtobase64}
+          />
+        </div>
+        <div className="fileBase64">
+          <textarea
+            placeholder="Enter base64 strings (one per line)"
+            cols={30}
+            rows={40}
+            onChange={handleBase64toPDF}
+          />
+          {iframeSrcs.map((src, index) => (
+            <iframe
+              key={index}
+              src={src}
+              title={`pdf-viewer-${index}`}
+              width={1440}
+              height={1024}
+            ></iframe>
+          ))}
+        </div>
       </div>
     </form>
   );
--- mini-project/src/services/CommonFunction.ts
+++ mini-project/src/services/CommonFunction.ts
@@ -1,3 +1,6 @@
+import { PDFDocument } from "pdf-lib";
+import { pdfjs } from "react-pdf";
+
 export function PDFtobase64(file: File): Promise<string | null> {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -16,4 +19,30 @@ export function PDFtobase64(file: File): Promise<string | null> {
     // อ่านไฟล์ PDF เป็น Data URL
     reader.readAsDataURL(file);
   });
+}
+
+export async function base64toPDF(base64Strings: string[]) {
+  pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+
+  const iframeSrcs = [];
+  try {
+    for (const base64String of base64Strings) {
+      const uint8Array = Uint8Array.from(atob(base64String), (c) =>
+        c.charCodeAt(0)
+      );
+
+      const pdfDoc = await PDFDocument.load(uint8Array);
+
+      //save the pdfDocument to a file
+      const pdfBytes = await pdfDoc.save();
+      const blob = new Blob([pdfBytes], { type: "application/pdf" });
+
+      const blobURL = URL.createObjectURL(blob);
+      iframeSrcs.push(blobURL);
+    }
+  } catch (error) {
+    console.error("Error processing base64 strings", error);
+  }
+
+  return iframeSrcs;
 }
